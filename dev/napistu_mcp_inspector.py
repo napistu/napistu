14ec3c168e930022b54688ac7a5ea28924a2ab33
Sean Hackett <seanmchackett@gmail.com>
2025-05-20 16:31:30 -0700

    working on serving and connecting to mcp

--- dev/napistu_mcp_inspector.py
+++ dev/napistu_mcp_inspector.py
@@ -9,8 +9,9 @@ from pathlib import Path
 import json
 
 # Import the MCP components
-from napistu.mcp.profiles import get_profile
-from napistu.mcp.server import create_server, start_server
+from napistu.mcp.profiles import get_profile, ServerProfile
+from napistu.mcp.server import create_server, initialize_components
+from mcp.server.fastmcp.server import FastMCP
 
 # Set up logging
 logging.basicConfig(level=logging.INFO)
@@ -21,10 +22,16 @@ session_context = {}
 object_registry = {}
 
 # define the types of assets to load
-profile = get_profile("full")
-
-mcp = create_server(profile)
+profile: ServerProfile = get_profile("full")
+mcp: FastMCP = create_server(profile)
 
 # This is needed for the MCP Inspector to work
 if __name__ == "__main__":
-    mcp.run(start_server(mcp))
+    """
+    Main entry point for the MCP Inspector script.
+
+    This script initializes all enabled MCP server components asynchronously, then starts the FastMCP server using stdio transport.
+    """
+    asyncio.run(initialize_components(mcp))
+    print("Initialization is complete!")
+    mcp.run("streamable-http")
